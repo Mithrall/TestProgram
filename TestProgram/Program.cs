a71c3cfae94f13795baafb3ff13dc12e9c320288
Kenneth Damkjær Nielsen <kenn6091@edu.eal.dk>
2017-10-05 12:54:37 +0200

    Add project files.

---
+++ TestProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProgram {
+    class Program {
+        static void Main(string[] args) {
+            Program myProgram = new Program();
+            myProgram.Run();
+        }
+        public void Run() {
+
+        }
+    }
+}
